eba2011d50a0ed5e683f7c0dafcf39a8e37d2d65
phn210 <57532941+phn210@users.noreply.github.com>
2021-12-04 04:23:56 +0700

    update frontend

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
+    document.getElementById('loading').style.visibility="hidden";
     // Navbar shrink function
     var navbarShrink = function() {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -69,5 +69,34 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     }
+
+    // Set the date we're counting down to
+var countDownDate = new Date("Dec 14, 2021 13:00:00 GMT-0000").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d : " + hours + "h : "
+  + minutes + "m : " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 });
