6e1b2994063fbfa722adb2a190221f53c4ba6da9
phn210 <57532941+phn210@users.noreply.github.com>
2021-12-14 16:50:05 +0700

    update correct mint time

--- js/scripts.js
+++ js/scripts.js
@@ -71,7 +71,7 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Dec 14, 2021 8:06:00 GMT-0000").getTime();
+    var countDownDate = new Date("Dec 14, 2021 13:00:00 GMT-0000").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
